609ee390c61084869643c3a2a8eea09b69eb3ed8
DharuNamikaze <dharunamikaze@gmail.com>
2024-11-07 15:22:18 +0530

    Font changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,17 @@
-// src/app/layout.js
-import './globals.css';
+// layout.jsx
 import { Poppins } from '@next/font/google';
 
 const poppins = Poppins({
-  subsets: ['latin'],   // Choose your subsets
-  weight: ['400', '700'] // Choose the font weights you need
+  subsets: ['latin'],
+  weight: ['400', '700'],
 });
-
-if (typeof window !== 'undefined') {
-  const savedTheme = localStorage.getItem('theme');
-  document.body.className = savedTheme === 'dark' ? 'dark' : 'light';
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {children}
+      </body>
     </html>
   );
 }
